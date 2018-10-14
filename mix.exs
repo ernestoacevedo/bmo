a63fc04ea296ea537a57af303624fad234957b51
Ernesto Acevedo <eacevedo@debian.cl>
2018-10-14 09:12:09 -0300

    Add coxir commander dependency

--- mix.exs
+++ mix.exs
@@ -24,7 +24,8 @@ defmodule Bmo.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:coxir, git: "https://github.com/satom99/coxir.git"}
+      {:coxir, git: "https://github.com/satom99/coxir.git"},
+      {:coxir_commander, git: "https://github.com/satom99/coxir_commander.git"}
     ]
   end
 end
